3fcafec93463b67694c663d52ff2df95cf34facc
Vitor Martinez <vtrmartinez@gmail.com>
2017-08-15 17:17:04 -0300

    Adding a new step

--- step_definitions/when.js
+++ step_definitions/when.js
@@ -1,4 +1,9 @@
 module.exports = function () {
     this.When('I click on Developer Guide', () =>
         browser.click("/html/body/nav/ul/li[3]/a")
-    )};
+    );
+
+    this.When('I click on menu Testrunner', () =>
+        browser.click("/html/body/section/div/section/nav/h3[3]/a")
+    );
+}
